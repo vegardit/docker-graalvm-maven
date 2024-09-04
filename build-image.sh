6f8ff3fcfadf1a2cbe0c48c6b85407e40116cf78
Emin Aktas <emin.aktas@axon-networks.com>
2024-09-04 15:57:33 +0200

    feat: add arm64 support (#21)
    * Creates new builder to build multi-arch container images
    * Creates local registery to push images local to run security audit
    before pushing image
    * Enables crane to copy images for multi-arch support
    * Adds a function to select architecture to support the links for
    multi-arch
    
    Signed-off-by: Emin Aktas <emin.aktas@axon-networks.com>

--- build-image.sh
+++ build-image.sh
@@ -20,28 +20,31 @@ docker_registry=${DOCKER_REGISTRY:-docker.io}
 image_repo=${DOCKER_IMAGE_REPO:-vegardit/graalvm-maven}
 image_tag="${DOCKER_IMAGE_TAG:-$graalvm_version-java$java_major_version}" # e.g. dev-java17, latest-java17, 22.3.2-java17
 
+
 #################################################
 # determine GraalVM download URL
 #################################################
 case $graalvm_version in
    dev)   graalvm_version=$(curl -fsSL -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/graalvm/graalvm-ce-dev-builds/releases/latest | grep "tag_name" | cut -d'"' -f4)
-          graalvm_url="https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/${graalvm_version}/graalvm-community-java${java_major_version}-linux-amd64-dev.tar.gz"
+          # {{ARCH}}, {{ARCH_1}}, {{ARCH_2}}, ..: Placeholders for architecture (e.g., x86_64, aarch64).
+          # get_arch_name in Dockerfile determines the correct architecture (amd64/arm64).
+          graalvm_url="https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/${graalvm_version}/graalvm-community-java${java_major_version}-linux-{{ARCH_3}}-dev.tar.gz"
           ;;
 
-   *dev*) graalvm_url="https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/${graalvm_version}/graalvm-community-java${java_major_version}-linux-amd64-dev.tar.gz"
+   *dev*) graalvm_url="https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/${graalvm_version}/graalvm-community-java${java_major_version}-linux-{{ARCH_3}}-dev.tar.gz"
           ;;
 
    latest) case $java_major_version in
               11) graalvm_version="22.3.3";
-                  graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${graalvm_version}/graalvm-ce-java11-linux-amd64-${graalvm_version}.tar.gz"
+                  graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${graalvm_version}/graalvm-ce-java11-linux-{{ARCH_3}}-${graalvm_version}.tar.gz"
                   ;;
               *)  graalvm_version=$(curl -sSfL -N https://github.com/graalvm/graalvm-ce-builds/tags | grep -oP "releases/tag/jdk-\K${java_major_version}.[^=\"]+" | head -n1)
-                  graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graalvm_version}/graalvm-community-jdk-${graalvm_version}_linux-x64_bin.tar.gz"
+                  graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graalvm_version}/graalvm-community-jdk-${graalvm_version}_linux-{{ARCH_2}}_bin.tar.gz"
                   ;;
            esac
            ;;
 
-   *)      graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graalvm_version}/graalvm-community-jdk-${graalvm_version}_linux-x64_bin.tar.gz"
+   *)      graalvm_url="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graalvm_version}/graalvm-community-jdk-${graalvm_version}_linux-{{ARCH_2}}_bin.tar.gz"
            image_tag="${DOCKER_IMAGE_TAG:-$graalvm_version}" # e.g. 17.0.7
            ;;
 esac
@@ -58,10 +61,19 @@ if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
    project_root=$(cygpath -w "$project_root")
 fi
 
-DOCKER_BUILDKIT=1 docker build "$project_root" \
+export DOCKER_BUILDKIT=1
+docker run -d -p 5000:5000 --name registryx registry
+local_registery=localhost:5000
+trap 'docker rm $(docker stop $(docker ps -a --filter ancestor=registryx --format="{{.ID}}"))' EXIT
+docker buildx create --name multiarchbuilder --driver docker-container --driver-opt network=host --bootstrap --platform linux/amd64,linux/arm64
+trap 'docker buildx rm multiarchbuilder' EXIT
+docker buildx build "$project_root" \
    --file "image/Dockerfile" \
    --progress=plain \
    --pull \
+   --builder multiarchbuilder \
+   --platform linux/amd64,linux/arm64 \
+   --push \
    `# using the current date as value for BASE_LAYER_CACHE_KEY, i.e. the base layer cache (that holds system packages with security updates) will be invalidate once per day` \
    --build-arg BASE_LAYER_CACHE_KEY=$base_layer_cache_key \
    --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
@@ -73,8 +85,15 @@ DOCKER_BUILDKIT=1 docker build "$project_root" \
    --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
    --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)" \
    --build-arg GITHUB_TOKEN="$GITHUB_TOKEN" \
-   -t $image_name \
+   -t $local_registery/$image_name \
    "$@"
+
+
+#################################################
+# pull the container image and retag for security audit and following tasks
+#################################################
+docker pull $local_registery/$image_name
+docker tag $local_registery/$image_name $image_name
 
 
 #################################################
@@ -89,16 +108,13 @@ fi
 # push image with tags to remote docker image registry
 #################################################
 if [[ "${DOCKER_PUSH:-0}" == "1" ]]; then
-   docker image tag $image_name $docker_registry/$image_name
-   docker push $docker_registry/$image_name
+   docker run --network host --rm gcr.io/go-containerregistry/crane copy $local_registery/$image_name $docker_registry/$image_name
 
    if [[ $graalvm_version != *dev* ]]; then
       if [[ $java_major_version == "11" ]]; then
-         docker image tag $image_name $docker_registry/$image_repo:$graalvm_version-java$java_major_version  # e.g. 22.3.2-java11
-         docker push $docker_registry/$image_repo:$graalvm_version-java$java_major_version
+         docker run --network host --rm gcr.io/go-containerregistry/crane copy $local_registery/$image_name $docker_registry/$image_repo:$graalvm_version-java$java_major_version  # e.g. 22.3.2-java11
       else
-         docker image tag $image_name $docker_registry/$image_repo:$graalvm_version # e.g. 17.0.7
-         docker push $docker_registry/$image_repo:$graalvm_version
+         docker run --network host --rm gcr.io/go-containerregistry/crane copy $local_registery/$image_name $docker_registry/$image_repo:$graalvm_version # e.g. 17.0.7
       fi
    fi
 fi
